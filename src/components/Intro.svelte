326cf117caf18c117a8851e881c5fda3ff8c0e4b
smolgeorgie <ilona.van.oosbree@hva.nl>
2025-07-10 12:13:13 +0200

    Introduction html base

---
+++ src/components/Intro.svelte
@@ -0,0 +1,15 @@
+<main>
+    <section class="hero">
+        <h1>Ilona van Oosbree</h1>
+        <p>Salutations! I'm Ilona van Oosbree, a Frontend Design and Development graduate since 2025 at the Amsterdam University of Applied Sciences and a proud wielder of the holy trinity: HTML, CSS, and JavaScript.</p>
+    </section>
+    
+    <article class="about-me">
+        <h2>About me</h2>
+        <p>I craft websites that are fast, inclusive, and don't fall apart when sneezed on. Whether I'm untangling some spaghetti code, nudging pixels into place with CSS, or giving semantic structure the love it deserves, I will always return to the basics. I am slightly obsessed with accessibility and performance, as I believe that everyone deserves to experience the same journey of the web.</p>
+    </article>
+    
+    <section class="call-to-action">
+        <p>Looking for a developer who knows her stuff, laughs at her own commit messages, and sprinkles accessibility like confetti That's me!</p>
+    </section>
+</main>
