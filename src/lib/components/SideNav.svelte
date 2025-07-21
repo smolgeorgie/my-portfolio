235246070488189f9371a9ff2f5e90ec086ac3b9
Ilona van Oosbree <144005439+smolgeorgie@users.noreply.github.com>
2025-07-21 17:06:34 +0200

    side-nav component + img

---
+++ src/lib/components/SideNav.svelte
@@ -0,0 +1,35 @@
+
+<div class="side-nav">
+    <div class="logo-container">
+        <img src="/images/ilona2.jpg" alt="Logo" class="logo" width="150" height="150">
+    </div>
+    <ul>
+        <li> <a href="#Email"> Email </a></li>
+        <li> <a href="#LinkedIn"> LinkedIn </a></li>
+    </ul>
+</div>
+
+<style> 
+    .side-nav {
+
+        ul {
+
+            list-style: none;
+
+            a {
+                text-decoration: none;
+                color: black;
+                display: block;
+            }
+        }
+    }
+
+    .logo-container {
+
+        img {
+            width: 250px;
+            height: 250px;
+            object-fit: cover;
+        }
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,10 @@
 <script>
     import Banner from '../lib/components/Banner.svelte';
+    import SideNav from '../lib/components/SideNav.svelte';
     import Intro from '../lib/components/Intro.svelte';
 </script>
 
+
 <Banner />
+<SideNav />
 <Intro />
