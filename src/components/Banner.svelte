be5735cc3bbd66b590b74c67c3c36f73f33790bf
Ilona van Oosbree <144005439+smolgeorgie@users.noreply.github.com>
2025-07-21 13:36:53 +0200

    Added banner + top-nav

---
+++ src/components/Banner.svelte
@@ -0,0 +1,22 @@
+<nav> 
+    <div class="banner">
+        <article class="banner-content"> 
+            <h1> Portfolio </h1>
+            <span> always in touch with your work </span>
+        </article>
+        <ul> 
+            <li> <a href="#Name"> [naam] </a></li>
+            <li> <a href="#Status"> Online </a></li>
+            <li> <a href="#Theme"> Verander thema </a></li>
+        </ul>
+    </div>
+    <div class="top-nav">
+            <ul> 
+                <li> <a href="#Home"> Home </a></li>
+                <li> <a href="#Profile"> Mijn profiel </a></li>
+                <li> <a href="#Work"> Mijn werk [amount] </a></li>
+                <li> <a href="#Foto"> Foto </a></li>
+                <li> <a href="#Blogs"> Blogs </a></li>
+            </ul>
+    </div>
+</nav>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,7 @@
 <script>
+    import Banner from '../components/Banner.svelte';
     import Intro from '../components/Intro.svelte';
 </script>
 
+<Banner />
 <Intro />
