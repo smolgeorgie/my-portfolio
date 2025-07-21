91032e1482ce94d29b4963ed432024ec59ef5f28
Ilona van Oosbree <144005439+smolgeorgie@users.noreply.github.com>
2025-07-21 17:28:27 +0200

    Added media query to side-nav

--- src/lib/blocks/SideNav.svelte
+++ src/lib/blocks/SideNav.svelte
@@ -14,7 +14,10 @@
 
         display: flex;
         flex-direction: column;
-        background: #94bce4;
+        background: #e9f3fc;
+        border-radius: 8px 8px 1px 1px;
+        box-shadow: 0px 2px 2px 2px rgba(0,0,0,0.1);
+        padding: 1rem;
 
         ul {
 
@@ -34,6 +37,34 @@
             width: 250px;
             height: 250px;
             object-fit: cover;
+        }
+    }
+
+    @media (max-width: 768px) { 
+        .side-nav {
+            align-items: center;
+            text-align: center;
+        }
+        
+        .logo-container {
+            order: -1; /* Ensures the logo stays at the top */
+            margin-bottom: 1rem;
+            
+            img {
+                width: 150px;
+                height: 150px;
+            }
+        }
+        
+        ul {
+            width: 100%;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            
+            li {
+                margin-bottom: 0.5rem;
+            }
         }
     }
 </style>
