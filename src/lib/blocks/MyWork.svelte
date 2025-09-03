2af1de7a46eb0cdc8b07c67eb1d98b6044e58951
Ilona van Oosbree <144005439+smolgeorgie@users.noreply.github.com>
2025-09-03 15:38:00 +0200

    Placed websites for portfolio

--- src/lib/blocks/MyWork.svelte
+++ src/lib/blocks/MyWork.svelte
@@ -5,15 +5,42 @@
        // Define your work items here
        const workPages = [
            {
-               id: 'portfolio',
+               id: 'creditcard',
                title: {
-                   nl: 'Portfolio',
-                   en: 'Portfolio'
+                   nl: 'Creditcard',
+                   en: 'Credit Card'
                },
                image: '/images/cool-darth.webp',
-               url: '/portfolio'
+               url: 'https://profile-card-s3-udja3.vercel.app/'
            },
-           // Add more work items as needed
+        
+           {
+               id: 'milledoni',
+               title: {
+                   nl: 'Milledoni',
+                   en: 'Milledoni'
+               },
+               image: '/images/cool-darth.webp',
+               url: 'https://milledoni.dev.fdnd.nl'
+           },
+              {
+                id: 'paerstudio',
+                title: {
+                     nl: 'Paer studio',
+                     en: 'Paer studio'
+                },
+                image: '/images/cool-darth.webp',
+                url: 'https://paerstudio.com/'
+              },
+              {
+                id: 'fdndwebsite',
+                title: {
+                     nl: 'FDND Website',
+                     en: 'FDND Website'
+                },
+                image: '/images/cool-darth.webp',
+                url: 'https://fdnd-programma.netlify.app/'
+              }
        ];
 </script>
 
